5d6502ae81124c7a9ac867b3808f27fa8272ad23
rdebroiz <rene-paul.debroize@irisa.fr>
2013-11-05 10:46:22 +0100

    Add comments.

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ Hello World plugin
 ==================
 
 This is a small example to show how to create a project for a plugin and to build it with the medinria-superproject build on the side.
-The plugin create a new workspace that allow the vizualiation of v3dView and a toolbox with a button which popup Hello world.
+The plugin create a new workspace that allow vizualiation and a toolbox with a button which popup hello world message box.
--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -28,31 +28,43 @@ public:
 
 };
 
+// constructor of the workspace
 helloworld::helloworld(QWidget *parent) : medWorkspace(parent), d(new helloworldPrivate)
 {
+    // Create a ToolBox and add it to the toolBox container of the workspace.
     d->helloworldToolBox = new medToolBox(parent);
     this->addToolBox(d->helloworldToolBox);
 
     d->helloworldToolBox->setTitle("The hello world toolbox");
 
+    // Fill the toolBox
     QWidget *helloworldToolBoxBody = new QWidget(d->helloworldToolBox);
     QPushButton *helloworldButton = new QPushButton("Click here", helloworldToolBoxBody);
     QVBoxLayout *helloworldToolBoxLayout =  new QVBoxLayout(helloworldToolBoxBody);
     helloworldToolBoxLayout->addWidget(helloworldButton);
     helloworldToolBoxBody->setLayout(helloworldToolBoxLayout);
-
     d->helloworldToolBox->addWidget(helloworldToolBoxBody);
 
+    // Connections
     connect(helloworldButton, SIGNAL(clicked()), this, SLOT(talkToTheWorld()));
 
 }
 
+// destructor
+helloworld::~helloworld(void)
+{
+    delete d;
+    d = NULL;
+}
+
+
+// Create a new tab in the view container of the workspace, where one can open views.
 void helloworld::setupViewContainerStack()
 {
     if (!stackedViewContainers()->count())
     {
         const QString description = this->description();
-        QString createdTab = addDefaultTypeContainer(description);
+        QString createdTab = addDefaultTypeContainer("Hello world tab");
         qDebug() << "Workspace" << this->description() << "created a new tab" << createdTab;
     }
 }
@@ -67,32 +79,28 @@ void helloworld::talkToTheWorld()
 }
 
 
-helloworld::~helloworld(void)
-{
-    delete d;
-    d = NULL;
-}
-
 QString helloworld::identifier() const {
     return "Hello world !!!";
 }
 
 QString helloworld::description() const {
-    return tr("  Would I say : 'Hello World !!!' ?  ");
+    return tr("Would I say : 'Hello World !!!' ?");
 }
 
 bool helloworld::isUsable(){
     return true;
 }
 
-
-
+// Your plugin could be dynamically load since you have implemented the registered methode.
+// It is call at the initialization of the plugin when the application start.
 bool helloworld::registered()
 {
+    // Here call a suitable factory, depending of the type of your plugin.
+    // (ie: workspace, process, data, dataSource, etc.)
     return medWorkspaceFactory::instance()->registerWorkspace
             <helloworld>(
                 "Hello world !!!",
                 "Hello world !!!",
-                "  Would I say : 'Hello World !!!' ?  "
+                "Would I say : 'Hello World !!!' ?"
                 );
 }
